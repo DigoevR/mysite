58183fb7268239541fb0a7b59ac4e7cbb595309f
DigoevR <digoevr@gmail.com>
2022-03-28 01:18:46 +0300

    Add POST

--- polls/urls.py
+++ polls/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-  path('', views.index, name='index'),
-  path('<int:question_id>/', views.detail, name='detail'),
-  path('<int:question_id>/results/', views.results, name='results'),
-  path('<int:question_id>/vote/', views.vote, name='vote'),
-  path('owner', views.owner, name='owner')
+  path('', views.Index.as_view(), name='index'),
+  path('<int:pk>/', views.Detail.as_view(), name='detail'),
+  path('<int:pk>/results/', views.Results.as_view(), name='results'),
+  path('<int:pk>/vote/', views.vote, name='vote'),
+  path('owner', views.owner, name='own er')
 ]
