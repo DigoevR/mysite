15a8d7e87961af198daf80e69b9aff3b6e7b2e67
rdigoev <rdigoev@mapsoft.by>
2022-04-17 22:39:37 +0300

    Change routing

--- ads/urls.py
+++ ads/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'ads'
 urlpatterns = [
     path('', views.AdsListView.as_view(), name='ad_list'),
-    path('<int:pk>', views.AdsDetailView.as_view(), name='ad_detail'),
+    path('ad/<int:pk>', views.AdsDetailView.as_view(), name='ad_detail'),
     path('create', views.AdsCreateView.as_view(), name='ad_create'),
     path('<int:pk>/update', views.AdsUpdateView.as_view(), name='ad_update'),
     path('<int:pk>/delete', views.AdsDeleteView.as_view(), name='ad_delete'),
